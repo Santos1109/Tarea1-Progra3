d386b1ffce6a7de623a7554a21818be7bd11bab1
santos1109 <santos1109@unitec.edu>
2017-07-27 07:11:31 -0600

    Tarea

---
+++ cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_b7a59.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_b7a59.dir/testCCompiler.c.obj"
+  "cmTC_b7a59.pdb"
+  "cmTC_b7a59.exe"
+  "cmTC_b7a59.exe.manifest"
+  "libcmTC_b7a59.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_b7a59.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,124 @@
+#include "Evaluador.h"
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+//Devuelve la suma de a (dado) y b (dado)
+int sumar(int a, int b)
+{
+    return a+b;
+}
+
+//Devuelve la resta de a (dado) y b (dado)
+int restar(int a, int b)
+{
+    return a-b;
+}
+
+//Devuelve la multiplicacion de a (dado) y b (dado)
+int multiplicar(int a, int b)
+{
+    return a*b;
+}
+
+//Devuelve la division de a (dado) y b (dado)
+int dividir(int a, int b)
+{
+    return a/b;
+}
+
+//Devuelve el numero mayor entre a (dado) y b (dado)
+int getMayor(int a, int b)
+{
+    if(a>b){
+        return a;
+    }
+
+    return b;
+}
+
+//Devuelve el numero mayor entre a (dado) y b (dado)
+int getMenor(int a, int b)
+{
+    if(a>b){
+      return b;
+    }
+
+    return a;
+
+
+
+}
+
+//Devuelve el numero mayor entre a (dado), b (dado) y c (dado)
+int getMayor(int a, int b, int c)
+{
+    if(a>b && a>c){
+        return a;
+    }else if( b>c){
+        return b;
+    }else{
+        return c;
+    }}
+
+
+
+//Establece el valor (dado) en el arreglo (dado) en el indice posicion (dado)
+void setValor(int arreglo[], int valor, int posicion)
+{
+    arreglo[posicion]=valor;
+}
+
+//Obtiene y devuelve el valor del arreglo (dado) en el indice posicion (dado)
+int getValor(int arreglo[], int posicion)
+{
+    int valor = arreglo[posicion];
+    return valor;
+}
+
+//Devuelve el numero mayor del arreglo (dado) que contiene tamano (dado) elementos
+int getMayor(int arreglo[], int tamano)
+{
+    int imin = std::numeric_limits<int>::min();
+
+    for(int i =0; i<tamano; i++){
+        if(arreglo[i]>imin){
+            imin=arreglo[i];
+
+        }
+    }
+    return imin;
+}
+
+//Devuelve el numero menor del arreglo (dado) que contiene tamano (dado) elementos
+int getMenor(int arreglo[], int tamano)
+{
+    int imax = std::numeric_limits<int>::max();
+
+    for(int i =0; i<tamano; i++){
+        if(arreglo[i]<imax){
+            imax=arreglo[i];
+        }
+    }
+    return imax;
+}
+
+//Devuelve el promedio de los numeros del arreglo (dado) que contiene tamano (dado) elementos
+int getPromedio(int arreglo[], int tamano)
+{
+    int acum=0;
+
+    for(int i =0; i<tamano; i++){
+         acum += arreglo[i];
+    }
+
+    return acum/tamano;
+}
+
+int main ()
+{
+    //Funcion evaluadora
+    evaluar();
+    return 0;
+}
